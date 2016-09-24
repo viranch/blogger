a3832327a16311b1513d7b4764a98487846b80ca
Viranch Mehta <email@viranch.me>
2016-09-24 23:22:16 +0530

    Separate Dockerfiles

--- destroy.sh
+++ destroy.sh
@@ -7,7 +7,7 @@ SIZE=$4
 
 terraform() {
     subcmd=$1; shift
-    docker run --rm -it -v $PWD/site-data/terraform:/data:ro -v $PWD/.state:/state -w /data build/blogger terraform $subcmd -state="/state/terraform.tfstate" "$@"
+    docker run --rm -it -v $PWD/site-data/terraform:/data:ro -v $PWD/.state:/state -w /data terraform $subcmd -state="/state/terraform.tfstate" "$@"
 }
 
 terraform destroy -var-file="$ENVI.tfvars" -var "server_size=$SIZE" -var "server_count=\"$N\"" -var "env=$ENVI" -var "app=$APP"
--- start.sh
+++ start.sh
@@ -8,19 +8,23 @@ SIZE=$4
 LOG_FILE="./.state/debug_`date +%FT%T`.log"
 
 echo "Building the builder image..."
-docker build -t build/blogger . 2>&1 >> $LOG_FILE || exit 1
+for app in terraform ansible; do
+    docker build -t $app -f Dockerfile.$app . || exit 1
+done 2>&1 >> $LOG_FILE
 
 terraform() {
     subcmd=$1; shift
-    docker run --rm -v $PWD/site-data/terraform:/data:ro -v $PWD/.state:/state -w /data build/blogger terraform $subcmd -state="/state/terraform.tfstate" "$@"
+    docker run --rm -v $PWD/site-data/terraform:/data:ro -v $PWD/.state:/state -w /data terraform $subcmd -state="/state/terraform.tfstate" "$@"
 }
 ansible-playbook() {
-    docker run --rm -v $PWD/site-data/ansible:/data:ro -v $PWD/.state:/state -w /data -e TERRAFORM_STATE_ROOT=/state -v $HOME/.ssh:/root/.ssh:ro build/blogger ansible-playbook "$@"
+    docker run --rm -v $PWD/site-data/ansible:/data:ro -v $PWD/.state:/state -w /data -e TERRAFORM_STATE_ROOT=/state -v $HOME/.ssh:/root/.ssh:ro ansible "$@"
 }
 
 echo "Spinning up instances..."
-terraform apply -var-file="$ENVI.tfvars" -var "server_size=$SIZE" -var "server_count=\"$N\"" -var "env=$ENVI" -var "app=$APP" 2>&1 >> $LOG_FILE || exit 1
-ansible-playbook -i terraform.py ./wait_for_instances.yml 2>&1 >> $LOG_FILE || exit 1
+(
+terraform apply -var-file="$ENVI.tfvars" -var "server_size=$SIZE" -var "server_count=\"$N\"" -var "env=$ENVI" -var "app=$APP" || exit 1
+ansible-playbook -i terraform.py ./wait_for_instances.yml || exit 1
+) 2>&1 >> $LOG_FILE
 
 echo "Setting up instances..."
 ansible-playbook -i terraform.py --ssh-extra-args="-o 'StrictHostKeyChecking=no'" ./playbook.yml 2>&1 >> $LOG_FILE || exit 1
