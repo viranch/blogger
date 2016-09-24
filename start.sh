70ed1cafa488ce97380f2f99fc69880c24ce3fb9
Viranch Mehta <email@viranch.me>
2016-09-24 20:56:47 +0530

    Fix terraform.py's state file discovery

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ terraform() {
     docker run --rm -v $PWD/site-data:/data -w /data build/blogger terraform $@
 }
 ansible-playbook() {
-    docker run --rm -v $PWD/site-data:/data -w /data -v $HOME/.ssh:/root/.ssh:ro build/blogger ansible-playbook $@
+    docker run --rm -v $PWD/site-data:/data -w /data -e TERRAFORM_STATE_ROOT=/data -v $HOME/.ssh:/root/.ssh:ro build/blogger ansible-playbook $@
 }
 
 echo "Spinning up instances..."
