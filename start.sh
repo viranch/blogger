ffc461b98103b27a22642de07d7f95f82325f734
Viranch Mehta <email@viranch.me>
2016-09-24 20:53:40 +0530

    Append to log file, not overwrite

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ SIZE=$4
 LOG_FILE="debug_`date +%FT%T`.log"
 
 echo "Building the builder image..."
-docker build -t build/blogger . 2>&1 > $LOG_FILE || exit 1
+docker build -t build/blogger . 2>&1 >> $LOG_FILE || exit 1
 
 terraform() {
     docker run --rm -v $PWD/site-data:/data -w /data build/blogger terraform $@
@@ -18,10 +18,10 @@ ansible-playbook() {
 }
 
 echo "Spinning up instances..."
-terraform apply -var-file="$ENVI.tfvars" -var "server_size=$SIZE" -var "server_count=\"$N\"" -var "env=$ENVI" -var "app=$APP" 2>&1 > $LOG_FILE || exit 1
-ansible-playbook -i terraform.py ./wait_for_instances.yml 2>&1 > $LOG_FILE || exit 1
+terraform apply -var-file="$ENVI.tfvars" -var "server_size=$SIZE" -var "server_count=\"$N\"" -var "env=$ENVI" -var "app=$APP" 2>&1 >> $LOG_FILE || exit 1
+ansible-playbook -i terraform.py ./wait_for_instances.yml 2>&1 >> $LOG_FILE || exit 1
 
 echo "Setting up instances..."
-ansible-playbook -i terraform.py --ssh-extra-args="-o 'StrictHostKeyChecking=no'" ./playbook.yml 2>&1 > $LOG_FILE || exit 1
+ansible-playbook -i terraform.py --ssh-extra-args="-o 'StrictHostKeyChecking=no'" ./playbook.yml 2>&1 >> $LOG_FILE || exit 1
 
 echo "Your blog is ready at http://"`terraform output elb_ip`"/"
